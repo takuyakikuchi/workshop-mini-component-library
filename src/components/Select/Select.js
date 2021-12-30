8a251cd37a085e26d5421c1be42e63272a80465b
Takuya Kikuchi <takuyakikuch@gmail.com>
2021-12-30 10:26:04 +0900

    feat: basic style

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,47 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const StyledSelect = styled.select`
+  appearance: none;
+  border: none;
+
+  height: 43px;
+  width: 191px;
+  border-radius: 8px;
+  padding: 12px 16px;
+  
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 1rem;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 16px;
+  margin: auto;
+  height: fit-content;
+  color: ${COLORS.gray700};
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+      <IconWrapper>
+        <Icon id="chevron-down" size={12} strokeWidth={2}/>
+      </IconWrapper>
+    </Wrapper>
   );
 };
 
