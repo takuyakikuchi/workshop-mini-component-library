129446885af8c054e7870aabb0a04ca8f33b04a9
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-01-01 11:46:32 +0900

    feat: reset default styling

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,20 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return (
+    <NativeInput type="text" id={label} name={label} placeholder={placeholder} width={width}/>
+  );
 };
+
+const NativeInput = styled.input`
+  padding: 0;
+  padding-right: 4px;
+  padding-bottom: 4px;
+  /* Leave space for icon */
+  padding-left: 16px;
+
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+`;
 
 export default IconInput;
